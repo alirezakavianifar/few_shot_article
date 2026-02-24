9c7b247d8ff9871ddff8dc6fef6c1f0a4e902654
Auto Commit <autocommit@example.com>
2026-02-23 23:31:41 -0800

    Cleanup: remove temp dump files, patch scripts, and zip backup

--- _dump2.py
+++
@@ -1,9 +0,0 @@
-import json
-nb = json.loads(open('dekae_colab.ipynb', encoding='utf-8').read())
-for cid, fname in [('b9806c81', 'cell_model.txt'), ('88ed9b71', 'cell_build.txt'), ('444b4113', 'cell_eval.txt')]:
-    for c in nb['cells']:
-        if c.get('id') == cid:
-            s = ''.join(c.get('source', ''))
-            open(fname, 'w', encoding='utf-8').write(s)
-            print(f'Written {fname}')
-            break
